787be38bebf66c412e0a5e4e43d0eb918d1e2562
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-02-15 23:32:45 -0500

    added page template

---
+++ app.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+
+// console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+    
+//     //This..
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//     }
+
+
+//     console.log('================');
+
+//     //is the same as this
+//     profileDataArr.forEach((profileItem) => {
+//         console.log(profileItem)
+//     });
+
+// };
+
+
+
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if(err) throw new Error(err);
+
+    console.log('Porfolio complete! Check out index.html to see the output!');
+});
